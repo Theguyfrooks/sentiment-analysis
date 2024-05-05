996ee0bd37309494e4e1e60958e7cf4ffd2a1e75
Farhan Ali <frooks3992@gmail.com>
2024-05-06 03:56:45 +0500

    s

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-df = pd.read_excel('/Users/mac/Downloads/tweets.xlsx')
+df = pd.read_excel('tweets.xlsx')
 
 
 from textblob import TextBlob
