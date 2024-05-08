4d26b37f64c6d91ac07d6a16b193a59e9728f07d
Farhan Ali <frooks3992@gmail.com>
2024-05-09 02:44:43 +0500

    filter-keywords

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-import streamlit as st
-import pandas as pd
-from textblob import TextBlob
-
-
-
-def has_keyword(tweet_text, keyword_array):
-    for keyword in keyword_array:
-        if keyword.lower() in tweet_text.lower():
-            return True
-    return False
-
-
-
-
-
 def analyze_sentiments(df, search_text):
     positive_count = 0
     negative_count = 0
     neutral_count = 0
 
-    keywords_filter = ["chor","gadha","bakwas","besharam","ullo ka pathha","ghatiya"]
+    keywords_filter = ["chor", "gadha", "bakwas", "besharam", "ullo ka pathha", "ghatiya"]
 
     total_count = len(df)
     search_text = search_text.lower()
 
     for text in df['Text']:
-      
         text = text.lower()
-
         if search_text in text:
-
             blob = TextBlob(text)
             sentiment_score = blob.sentiment.polarity
             if sentiment_score > 0:
@@ -37,21 +18,14 @@ def analyze_sentiments(df, search_text):
             elif sentiment_score < 0:
                 negative_count += 1
             else:
-
-        if has_keyword(df["Text"], keywords_filter):
-
-                negative_count += 1
-
-             else:
-
-
-                neutral_count += 1
-    
+                if has_keyword(text, keywords_filter):
+                    negative_count += 1
+                else:
+                    neutral_count += 1
 
     positive_percentage = (positive_count / total_count) * 100
     negative_percentage = (negative_count / total_count) * 100
     neutral_percentage = (neutral_count / total_count) * 100
-
 
     total_percentage = positive_percentage + negative_percentage + neutral_percentage
     if total_percentage != 100:
@@ -60,22 +34,6 @@ def analyze_sentiments(df, search_text):
         negative_percentage *= scale_factor
         neutral_percentage *= scale_factor
 
-
-    # st.write("Total tweets: ", total_count)
     st.write("Positive Sentiments:", positive_count, f" ({positive_percentage:.2f}%)")
     st.write("Negative Sentiments:", negative_count, f" ({negative_percentage:.2f}%)")
     st.write("Neutral Sentiments:", neutral_count, f" ({neutral_percentage:.2f}%)")
-
-
-
-
-# Upload Excel file
-uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
-
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.write(df.head())
-    
-    search_text = st.text_input("Search Text:")
-    if st.button("Analyze"):
-        analyze_sentiments(df, search_text)
