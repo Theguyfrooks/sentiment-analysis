acae67fa9a6ec306afcc277bd186df9686cd0602
Farhan Ali <frooks3992@gmail.com>
2024-05-09 02:51:37 +0500

    filter_fixed

--- main.py
+++ main.py
@@ -13,17 +13,14 @@ def analyze_sentiments(df, search_text):
     negative_count = 0
     neutral_count = 0
 
-    keywords_filter = ["chor","gadha","bakwas","besharam","ullo ka pathha","ghatiya"]
+    keywords_filter = ["chor", "gadha", "bakwas", "besharam", "ullo ka pathha", "ghatiya"]
 
     total_count = len(df)
     search_text = search_text.lower()
 
     for text in df['Text']:
-      
         text = text.lower()
-
         if search_text in text:
-
             blob = TextBlob(text)
             sentiment_score = blob.sentiment.polarity
             if sentiment_score > 0:
@@ -31,21 +28,14 @@ def analyze_sentiments(df, search_text):
             elif sentiment_score < 0:
                 negative_count += 1
             else:
-
-        if has_keyword(df["Text"], keywords_filter)
-
-                negative_count += 1
-
-             else:
-
-
-                neutral_count += 1
-    
+                if has_keyword(text, keywords_filter):
+                    negative_count += 1
+                else:
+                    neutral_count += 1
 
     positive_percentage = (positive_count / total_count) * 100
     negative_percentage = (negative_count / total_count) * 100
     neutral_percentage = (neutral_count / total_count) * 100
-
 
     total_percentage = positive_percentage + negative_percentage + neutral_percentage
     if total_percentage != 100:
@@ -54,8 +44,6 @@ def analyze_sentiments(df, search_text):
         negative_percentage *= scale_factor
         neutral_percentage *= scale_factor
 
-
-    # st.write("Total tweets: ", total_count)
     st.write("Positive Sentiments:", positive_count, f" ({positive_percentage:.2f}%)")
     st.write("Negative Sentiments:", negative_count, f" ({negative_percentage:.2f}%)")
     st.write("Neutral Sentiments:", neutral_count, f" ({neutral_percentage:.2f}%)")
